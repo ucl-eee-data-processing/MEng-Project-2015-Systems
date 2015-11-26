30936fbe047619a7ab646e37df08ec90670e2ffc
Timothy Okwii <tokwii@cisco.com>
2015-11-26 14:18:58 +0000

    Create skeletal calss for the Minstry of Energy data processor
    Related to issue:#1

---
+++ pre_processor/mot_processor.py
@@ -0,0 +1,22 @@
+import wget
+import time
+import csv
+
+class MinistryOfTransportDatasetProcessor(object):
+
+    def __init__(self):
+        self.base_url = ''
+        pass
+
+    def _format_data(self):
+        pass
+
+    def _delete_files(self):
+        pass
+
+    def replay(self,from_date):
+        # Replays the data from a certain date to today
+        pass
+
+    def publish_data(self):
+        pass
