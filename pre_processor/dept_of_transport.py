a8e34bfe44e26d49876cf7113f48e171472497d9
Timothy Okwii <tokwii@cisco.com>
2015-12-11 19:36:22 +0000

    Added method to replay data
    Related to issue:#1

--- pre_processor/dept_of_transport.py
+++ pre_processor/dept_of_transport.py
@@ -4,6 +4,7 @@ import datetime
 import csv
 import glob
 from time import strptime
+from calendar import monthrange
 
 PROCESSOR_DIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -12,20 +13,17 @@ class DepartmentOfTransportDatasetProcessor(object):
 
     def __init__(self, offset=None, year=None, month=None):
         self.base_url = 'http://www.ecodriver.uk.com/eCMS/Files/DFT/'
-        self.offset = offset
-        self.month = month
-        self.year = year 
 
-    def _serialize(self):
-        pass
+    def _generate_filename(self, month=None, year=None):
 
-    def _generate_filename(self):
-        # TODO add defualts replay might need this too
-        time_now = datetime.datetime.now()
-        month = time_now.strftime('%B')[:3].lower()
-        year = time_now.year
-        filename = 'deptfortransport_' + month + '-' + str(year) + '.csv'
-        return filename
+        if month == None and year == None:
+            time_now = datetime.datetime.now()
+            month = time_now.strftime('%B')[:3].lower()
+            year = time_now.year
+            filename = 'deptfortransport_' + month + '-' + str(year) + '.csv'
+            return filename
+        else:
+            return 'deptfortransport_' + month + '-' + str(year) + '.csv'
 
     def _current_dataset_month(self):
         dataset = ''
@@ -39,8 +37,6 @@ class DepartmentOfTransportDatasetProcessor(object):
             return None
 
     def _get_datapoint(self, filename, date, time):
-        print time
-        print date
         data_list = []
         data_points = {}
         with open(filename, 'rb') as csvfile:
@@ -52,17 +48,39 @@ class DepartmentOfTransportDatasetProcessor(object):
             data_points.update({data['Meter']: data[time]})
         return data_points
 
-    def download_dataset(self):
-        # TODO check whether file exist
-        link_address = self.base_url + self._generate_filename()
+    def _download_dataset(self, filename):
+        link_address = self.base_url + filename
         wget.download(link_address)
 
     def remove_dataset(self, filename):
         pass
 
-    def replay(self,from_date):
-        # Replays the data from a certain date to today
-        pass
+    def replay(self, month, year):
+        filename = self._generate_filename(month=month, year=year)
+        numerical_month = strptime(month,'%b').tm_mon
+        month_range = monthrange(year, numerical_month)
+        month_data = {}
+        if os.path.isfile(PROCESSOR_DIR + '/' + filename):
+            for iter_date in range(month_range[0], month_range[1] +1):
+                # Month is hard coded
+                date = str(iter_date) + '/' + '11' + '/' + str(year)
+                date_data = {}
+                for iter_minutes in range(0,1440,30):
+                    hour = iter_minutes // 60
+                    minutes = iter_minutes % 60
+                    if len(str(hour)) == 1:
+                        hour = '0' + str(hour)
+                    if len(str(minutes)) == 1:
+                        minutes = '0' + str(minutes)
+                    time = str(hour) + ':' + str(minutes)
+                    datapoint = self._get_datapoint(filename ,date, time)
+                    date_data.update({time: datapoint})
+                month_data.update({date: date_data})
+            return month_data            
+        else:
+            self._download_dataset(filename)
+
+
 
     def publish_real_time_data(self):
         time = datetime.datetime.now().strftime('%d/%m/%y %H:%M')
@@ -81,10 +99,12 @@ class DepartmentOfTransportDatasetProcessor(object):
         else:
             pass 
             # Remove the old one and download the lastest one
+            # There is no current data 
 
 if __name__ == '__main__':
     dataset = DepartmentOfTransportDatasetProcessor()
     #print dataset._time_current_dataset()
-    dataset.publish_real_time_data()
+    #dataset.publish_real_time_data()
+    print dataset.replay('dec',2015)
 
 
