f3035b1049882b932f47ab741eaf70b9ccabdfde
Timothy Okwii <tokwii@cisco.com>
2016-02-23 10:51:31 +0000

    Updated Spark Playbook

--- tools/test_kafka.py
+++ tools/test_kafka.py
@@ -2,7 +2,7 @@ from kafka import SimpleConsumer, SimpleClient
 
 # To consume messages
 client = SimpleClient('192.168.33.30:9092')
-consumer = SimpleConsumer(client, "my-group", "OryxInput",max_buffer_size=1000000)
+consumer = SimpleConsumer(client, "my-group", "OryxUpdate",max_buffer_size=1000000)
 for message in consumer:
     # message is raw byte string -- decode if necessary!
     # e.g., for unicode: `message.decode('utf-8')`
