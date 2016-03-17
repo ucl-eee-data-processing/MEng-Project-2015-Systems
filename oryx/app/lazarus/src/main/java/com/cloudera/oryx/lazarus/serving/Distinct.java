d55989cca3bf931063bd1d91a5e18bed0058bb4c
Timothy Okwii <tokwii@cisco.com>
2016-03-17 12:44:56 +0000

    Debug
    Related to issue:#10
    Please enter the commit message for your changes. Lines starting

--- oryx/app/lazarus/src/main/java/com/cloudera/oryx/lazarus/serving/Distinct.java
+++ oryx/app/lazarus/src/main/java/com/cloudera/oryx/lazarus/serving/Distinct.java
@@ -42,7 +42,7 @@ public final class Distinct extends OryxResource {
   @Path("{word}")
   public int get(@PathParam("word") String word) {
     Integer count = getModel().getWords().get(word);
-    System.out.println("Hellooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo");
+    //System.out.println("Hellooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo");
     System.out.println("Model");
     System.out.println(getModel());
     return count == null ? 0 : count;
